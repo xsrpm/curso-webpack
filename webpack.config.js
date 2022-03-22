7c70eb0f3d7460ce299e5404ab8b4e9c10a6a54a
César Palma Morante <cemp2703@gmail.com>
2022-03-22 07:21:11 -0500

    adding html webpack plugin and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
@@ -12,16 +13,19 @@ module.exports = {
   },
   module :{
     rules:[
-    {
-        test: /\.(mjs|js)$/,
-        exclude: /node_modules/,
-        use:{
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    }
-]
-}
+      {
+          test: /\.(mjs|js)$/,
+          exclude: /node_modules/,
+          use:{
+              loader: 'babel-loader',
+              options: {
+                  presets: ['@babel/preset-env']
+              }
+          }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "./public/index.html"),
+  })],
 };
